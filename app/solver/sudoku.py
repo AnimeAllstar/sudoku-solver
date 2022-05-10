f95824437579c817032aabc9a7a5f6d7b5f86a1f
Asad Dhorajiwala <asadrehandhorajiwala@gmail.com>
2022-05-11 00:20:31 +0530

    Moved solver to ./app

---
+++ app/solver/sudoku.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+
+class Sudoku:
+    def __init__(self, grid=None):
+        self.solution = None
+        self.N = 9
+
+        if grid is not None:
+            if isinstance(grid, np.ndarray):
+                self.grid = grid
+            else:
+                self.grid = np.array(grid)
+
+            if self.grid.shape != (self.N, self.N):
+                raise ValueError(f"grid must be {self.N}x{self.N}")
+
+            self.unsolved = self.grid.copy()
+        else:
+            self.grid = np.zeros((self.N, self.N), dtype=int)
+
+    def set(self, row, col, num):
+        self.grid[row][col] = num
+
+    def get(self, row, col):
+        return self.grid[row][col]
+
+    def solve(self):
+        if self.solution is None:
+            self.unsolved = self.grid.copy()
+        if self.check_solvable(0, 0):
+            self.solution = self.grid.copy()
+            return True
+        return False
+
+    def check_solvable(self, row, col):
+        if row == self.N - 1 and col == self.N:
+            return True
+        if col == self.N:
+            row += 1
+            col = 0
+        if self.get(row, col) > 0:
+            return self.check_solvable(row, col + 1)
+        for num in range(1, self.N + 1, 1):
+            if self.check_solvable_helper(row, col, num):
+                self.set(row, col, num)
+                if self.check_solvable(row, col + 1):
+                    return True
+            self.set(row, col, 0)
+        return False
+
+    def check_solvable_helper(self, row, col, num):
+        for x in range(9):
+            if self.get(row, x) == num:
+                return False
+
+        for x in range(9):
+            if self.get(x, col) == num:
+                return False
+
+        startRow = row - row % 3
+        startCol = col - col % 3
+        for i in range(3):
+            for j in range(3):
+                if self.get(i + startRow, j + startCol) == num:
+                    return False
+        return True
+
+    def solution(self):
+        return self.solution
+
+    def show(self, solution=False):
+        if solution:
+            print(self.solution)
+        else:
+            print(self.unsolved)
---
+++ app/solver/test.py
@@ -0,0 +1,24 @@
+from sudoku import Sudoku as py_Sudoku
+from solver.sudoku import Sudoku
+
+
+def test():
+    puzzle = py_Sudoku(3).difficulty(0.9)
+    print("empty grid:")
+    puzzle.show()
+    solution = puzzle.solve()
+    print("py_Sudoku solution:")
+    solution.show()
+    grid = None_to_zero(puzzle.board.copy())
+    sudoku = Sudoku(grid)
+    sudoku.solve()
+    print("our solution:")
+    sudoku.show(solution=True)
+
+
+def None_to_zero(grid):
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] == None:
+                grid[i][j] = 0
+    return grid
