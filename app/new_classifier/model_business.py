3b422781775b1ef4a625fc2e1366c8ffa9352ea1
Taylor Leong <imwhoiem@gmail.com>
2022-05-17 01:54:55 -0700

    split TMNIST to train and test data

--- app/new_classifier/model_business.py
+++ app/new_classifier/model_business.py
@@ -1,17 +1,31 @@
 import tensorflow as tf
 import cv2 as cv
-from our_classifier.digit_classifier import DigitClassifier
+import numpy as np
+import pandas as pd
+from new_classifier.digit_classifier import DigitClassifier
 from utils.utils import read_img
+from sklearn.model_selection import train_test_split 
 
 
 def train_model():
-    mnist = tf.keras.datasets.mnist
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
-    X_train = X_train.reshape(-1, 28, 28, 1).astype('float32')
+    df = pd.read_csv("./our_classifier/training_data/TMNIST_Data.csv")
+    df_train, df_test = train_test_split(df, test_size=int(df.shape[0] / 4))
+    X_train = df_train.drop(columns={"names", "labels"})
+    y_train = df_train[["labels"]]
+    X_train = X_train.values.reshape(-1, 28, 28, 1).astype('float32')
     X_train = X_train / 255.0
 
+    mnist = tf.keras.datasets.mnist
+    (X_train_2, y_train_2), (X_test_2, y_test) = mnist.load_data()
+    X_train_2 = X_train_2.reshape(-1, 28, 28, 1).astype('float32')
+    X_train_2 = X_train_2 / 255.0
+
+    y_train_2 = y_train_2.reshape(-1, 1)
+    X_train = np.concatenate((X_train, X_train_2))
+    y_train = np.concatenate((y_train, y_train_2))
     model = DigitClassifier()
     model.fit(X_train, y_train)
+
 
 
 def test_model():
@@ -25,10 +39,21 @@ def test_model():
 
 
 def eval_model():
-    mnist = tf.keras.datasets.mnist
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
-    X_test = X_test.reshape(-1, 28, 28, 1).astype('float32')
+    df = pd.read_csv("./our_classifier/training_data/TMNIST_Data.csv")
+    df_train, df_test = train_test_split(df, test_size=int(df.shape[0] / 4))
+    X_test = df_test.drop(columns={"names", "labels"})
+    y_test = df_test[["labels"]]
+    X_test = X_test.values.reshape(-1, 28, 28, 1).astype('float32')
     X_test = X_test / 255.0
+
+    mnist = tf.keras.datasets.mnist
+    (X_train_2, y_train_2), (X_test_2, y_test_2) = mnist.load_data()
+    X_test_2 = X_test_2.reshape(-1, 28, 28, 1).astype('float32')
+    X_test_2 = X_test_2 / 255.0
+
+    y_test_2 = y_test_2.reshape(-1, 1)
+    X_test = np.concatenate((X_test, X_test_2))
+    y_test = np.concatenate((y_test, y_test_2))
     model = DigitClassifier()
     model.evaluate(X_test, y_test)
 
